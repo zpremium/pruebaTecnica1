5712e8934d7188d3672a45178b28fd200e949526
zpremium <zdravkoms99@gmail.com>
2021-06-10 14:36:49 +0200

    logic of basket done

---
+++ pruebaTecnica1/src/app/interface/CartProduct.ts
@@ -0,0 +1,8 @@
+export interface CartProduct {
+  id:string;
+  name:string;
+  price:number;
+  quantity:number;
+  discount:number;
+  totalPriceWithDiscount:number;
+}
--- pruebaTecnica1/src/app/shop/shop.component.ts
+++ pruebaTecnica1/src/app/shop/shop.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../interface/products';
 import { PRODUCTS } from '../data/mock-products';
-
+import { CartProduct } from '../interface/CartProduct';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-shop',
@@ -10,9 +11,9 @@ import { PRODUCTS } from '../data/mock-products';
   styleUrls: ['./shop.component.css'],
 })
 export class ShopComponent implements OnInit {
-
-  products=PRODUCTS;
-  product?: Product[];
+  products = PRODUCTS;
+  product: Product[] = [];
+  cart: CartProduct[] = [];
 
   constructor(private route: Router) {}
 
@@ -20,5 +21,29 @@ export class ShopComponent implements OnInit {
 
   btnClick() {
     this.route.navigateByUrl('/basket');
+  }
+
+  addNew(product: Product) {
+    //si el producto no esta selecionado, lo añadimos y si esta selecionado hacemos sumatorio (se suma +1 quantity)
+    let productCart = this.cart.find(
+      (elementCart) => elementCart.id === product.id
+    );
+    if (productCart === undefined) {
+      // this.cart.push(product);
+      // } else {
+      //   productCart.quantity === 1;
+      let CartProductToAdd = {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        quantity:1,
+        discount:0,
+        totalPriceWithDiscount:0,
+      };
+      this.cart.push(CartProductToAdd)
+    }else{
+      productCart.quantity +=1
+    }
+    console.log(product);
   }
 }
